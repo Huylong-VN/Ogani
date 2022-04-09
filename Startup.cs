e8938e4db20a6b2a08e1107419c6e2c36854183d
Anh Huy <73161051+Huylong-VN@users.noreply.github.com>
2022-04-09 09:11:05 +0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -93,8 +93,9 @@ namespace Ogani
                 // The default HSTS value is 30 days. You may want to change this for production
                 // scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
-            app.UseDeveloperExceptionPage();
+          
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
